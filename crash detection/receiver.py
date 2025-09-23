05382baaa31a53f3586c7184e2fba1fd7c3b7f31
Tigi <61912726+content@users.noreply.github.com>
2025-09-23 12:04:06 +0200

    feat: Add WebSocket receiver and integrate with script
    Introduces receiver.py to handle WebSocket messages and trigger key presses via pyautogui. Updates script.py to send a disconnect message over WebSocket when the CS2 client count is insufficient, replacing the direct key press. Adds a 'port' field to config.json for configurable WebSocket communication.

---
+++ crash detection/receiver.py
@@ -0,0 +1,34 @@
+import asyncio
+import websockets
+import pyautogui
+import json
+
+config = {}
+with open("config.json", "r", encoding="utf-8") as f:
+    config = json.load(f)
+
+DISCONNECT_KEY = "k"
+
+HOST = "127.0.0.1"
+PORT = config["port"]
+
+async def handler(ws):
+    print("[INFO] Sender connected")
+    try:
+        async for msg in ws:
+            print(f"[INFO] Received message: {msg}")
+
+            if msg == "disconnect":
+                pyautogui.press(DISCONNECT_KEY)
+
+    except websockets.ConnectionClosed:
+        print("[INFO] Sender disconnected")
+
+
+async def main():
+    async with websockets.serve(handler, HOST, PORT):
+        print(f"[INFO] Receiver running at ws://{HOST}:{PORT}")
+        await asyncio.Future()
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- crash detection/script.py
+++ crash detection/script.py
@@ -1,15 +1,19 @@
 from discord_webhook import DiscordWebhook
 import pyautogui, time, psutil, json
+import asyncio
+import websockets
 
 config = {}
 with open("config.json", "r", encoding="utf-8") as f:
     config = json.load(f)
 
+HOST = "127.0.0.1"
+PORT = config["port"]
+
 discord_id = config["discord_id"]
 webhook_url = config["webhook_url"]
 
 expected_cs2_count = 15
-key_to_press = "end"
 
 cs2_process_name = "cs2.exe"
 cs2_pids = []
@@ -17,17 +21,25 @@ cs2_pids = []
 has_sent_alert = False
 
 print("[INFO] Script has been started.")
+
+async def send_ws_message(message: str):
+    uri = f"ws://{HOST}:{PORT}"
+    try:
+        async with websockets.connect(uri) as ws:
+            await ws.send(message)
+    except Exception as e:
+        print(f"[ERROR] Failed to send WebSocket message: {e}")
+
 while True:
     cs2_pids = [p.pid for p in psutil.process_iter() if p.name().lower() == cs2_process_name]
     cs2_client_count = len(cs2_pids)
 
     if cs2_client_count != expected_cs2_count:
-        pyautogui.press(key_to_press)
-
         if not has_sent_alert:
             webhook = DiscordWebhook(url=webhook_url, content=f"<@{discord_id}> Not enough CS2 clients! Current count: {len(cs2_pids)} (Expected: {expected_cs2_count})")
             response = webhook.execute()
 
+            asyncio.run(send_ws_message(f"disconnect"))
             has_sent_alert = True
 
             print("[INFO] Not enough CS2 clients! Sent alert to Discord.")
